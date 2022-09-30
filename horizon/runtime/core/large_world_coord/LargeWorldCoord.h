dc3713f7505a1f6a153da3f966db4055f0f9a29c
v4vendeta <hy.lu@outlook.com>
2022-09-30 22:03:04 +0800

    m

---
+++ horizon/runtime/core/large_world_coord/LargeWorldCoord.h
@@ -0,0 +1,16 @@
+#define MAKE_LWC(x) 
+
+LWC::float4();
+LWC::float3();
+LWC::float2();
+
+namespace Horizon::LargeWorldCoord{
+
+static constexpr f32 LWC_TILE_SIZE = 10000.0f; // 10 km
+
+template <typename T>
+class LWCVector{
+
+    T
+};
+}
