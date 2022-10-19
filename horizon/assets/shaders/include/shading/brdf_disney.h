8fb9144563a55c18e7b913459d74216fbb8f9070
v4vendeta <hy.lu@outlook.com>
2022-10-20 02:02:29 +0800

    disney12

--- horizon/assets/shaders/include/shading/brdf_disney.h
+++ horizon/assets/shaders/include/shading/brdf_disney.h
@@ -36,19 +36,31 @@ https://github.com/wdas/brdf/blob/425f6ef183f3e57c2a351622790ca8ea472ddfe1/src/b
 //     float clearcoatGloss;
 // };
 
+// color baseColor .82 .67 .16
+// float metallic 0 1 0
+// float subsurface 0 1 0
+// float specular 0 1 .5
+// float roughness 0 1 .5
+// float specularTint 0 1 0
+// float anisotropic 0 1 0
+// float sheen 0 1 0
+// float sheenTint 0 1 .5
+// float clearcoat 0 1 0
+// float clearcoatGloss 0 1 1
+
 /*
-explaination: 
+explaination:
 this brdf coupled smooth and rough mat.subsurface scattering model
 consider the refaction incident and outgoing the surface (smooth)
 consider the retroreflection increase by mat.roughness at grazing angle(rough)
 
-if fd90 > 1.0, diffuse increase at grazing angle. 
+if fd90 > 1.0, diffuse increase at grazing angle.
 if fd90 > 1.0, diffuse increase at grazing angle.
 */
 float3 DiffuseBurley12(float3 albedo, float roughness, float LoH, float NoL, float NoV) {
     float fd90 = 0.5f + 2.0 * roughness * LoH * LoH;
     float f_theta_l = 1.0 + (fd90 - 1.0) * Pow5(1.0 - NoL); // incident light refractiion
-    float f_theta_v =  1.0 + (fd90 - 1.0) * Pow5(1.0 - NoV); // outgoing light refraction
+    float f_theta_v = 1.0 + (fd90 - 1.0) * Pow5(1.0 - NoV); // outgoing light refraction
     return albedo * _1DIVPI * f_theta_v * f_theta_l;
 }
 
@@ -65,70 +77,57 @@ float3 SubsurfaceBurley12(float3 albedo, float roughness, float NoL, float NoV, 
 float NDF_GTR_GAMMA(float NoH, float a, float gamma) {
     float k;
     float a2 = a * a;
-    if(a == 1.0) {
+    if (a == 1.0) {
         k = 1.0;
-    } else if(gamma == 1.0 && a != 1.0) {
+    } else if (gamma == 1.0 && a != 1.0) {
         k = (a2 - 1.0) / log(a2);
-    } else if(gamma != 1.0 && a != 1.0) {
+    } else if (gamma != 1.0 && a != 1.0) {
         k = (gamma - 1.0) * (a2 - 1.0) / (1.0 - pow(a2, 1.0 - gamma));
     }
     float denom = pow(1.0 + Pow2(NoH) * (a2 - 1.0), gamma);
     return k / denom;
 }
 
-float NDF_GTR1(float NdotH, float a)
-{
-    if (a >= 1) return 1/_PI;
-    float a2 = a*a;
-    float t = 1 + (a2-1)*NdotH*NdotH;
-    return (a2-1) / (_PI*log(a2)*t);
+float NDF_GTR1(float NdotH, float a) {
+    if (a >= 1)
+        return 1 / _PI;
+    float a2 = a * a;
+    float t = 1 + (a2 - 1) * NdotH * NdotH;
+    return (a2 - 1) / (_PI * log(a2) * t);
 }
 
-float NDF_GTR2(float NdotH, float a)
-{
-    float a2 = a*a;
-    float t = 1 + (a2-1)*NdotH*NdotH;
-    return a2 / (_PI * t*t);
+float NDF_GTR2(float NdotH, float a2) {
+    float t = 1 + (a2 - 1) * NdotH * NdotH;
+    return a2 / (_PI * t * t);
 }
 
-float NDF_GTR2_Aniso(float NoH, float XoH, float YoH, float ax, float ay)
-{
-    return 1 / (_PI * ax*ay * Pow2( Pow2(XoH/ax) + Pow2(YoH/ay) + NoH*NoH ));
+float NDF_GTR2_Aniso(float NoH, float XoH, float YoH, float ax, float ay) {
+    return 1 / (_PI * ax * ay * Pow2(Pow2(XoH / ax) + Pow2(YoH / ay) + NoH * NoH));
 }
 
-float Vis_smithG_GGX_Aniso(float NdotV, float VdotX, float VdotY, float ax, float ay)
-{
-    return 1 / (NdotV + Pow2t( Pow2(VdotX*ax) + Pow2(VdotY*ay) + Pow2(NdotV) ));
+float Vis_Smith_GGX(float NdotV, float alphaG) {
+    float a = alphaG * alphaG;
+    float b = NdotV * NdotV;
+    return 1 / (NdotV + SqrtFast2(a + b - a * b));
 }
 
+float Vis_Smith_GGX_Aniso(float NdotV, float VdotX, float VdotY, float ax, float ay) {
+    return 1 / (NdotV + SqrtFast2(Pow2(VdotX * ax) + Pow2(VdotY * ay) + Pow2(NdotV)));
+}
 
-float3 Sheen_Burley12(float3 albedo, float sheen_tint, float sheen, float3 fresnel) {
-    float l = Luminance(albedo); // luminance approx.
-    float3 c_tint = l > 0.0 ? albedo / l: float3(1.0, 1.0, 1.0); // normalize lum. to isolate hue+sat
+float3 Sheen_Burley12(float3 albedo, float sheen_tint, float sheen, float f) {
+    float l = Luminance(albedo);                                  // luminance approx.
+    float3 c_tint = l > 0.0 ? albedo / l : float3(1.0, 1.0, 1.0); // normalize lum. to isolate hue+sat
     float3 c_sheen = mix(float3(1), c_tint, sheen_tint);
-    return fresnel * sheen * c_sheen * albedo * _1DIVPI;
+    return f * sheen * c_sheen;
 }
 
-// color baseColor .82 .67 .16
-// float metallic 0 1 0
-// float subsurface 0 1 0
-// float specular 0 1 .5
-// float roughness 0 1 .5
-// float specularTint 0 1 0
-// float anisotropic 0 1 0
-// float sheen 0 1 0
-// float sheenTint 0 1 .5
-// float clearcoat 0 1 0
-// float clearcoatGloss 0 1 1
-
-
-// float smithG_GGX(float NdotV, float alphaG)
-// {
-//     float a = alphaG*alphaG;
-//     float b = NdotV*NdotV;
-//     return 1 / (NdotV + Pow2t(a + b - a*b));
-// }
-
+float3 ClearCoat_Burley12(float NoL, float NoV, float NoH, float clearcoat, float clearcoat_gloss) {
+    float Dr = NDF_GTR1(NoH, lerp(0.1, 0.001, clearcoat_gloss));
+    float Fr = lerp(0.04, 1.0, Pow5(1.0 - NoH));
+    float Gr = Vis_Smith_GGX(NoL, 0.25) * Vis_Smith_GGX(NoV, 0.25);
+    return float3(0.25 * clearcoat * Dr * Fr * Gr);
+}
 
 // float3 BRDF(MaterialProperties_Disney12 mat, float3 L, float3 V, float3 N, float3 X, float3 Y )
 // {
@@ -184,12 +183,11 @@ float3 Sheen_Burley12(float3 albedo, float sheen_tint, float sheen, float3 fresn
 //         + Gs*Fs*Ds + .25*mat.clearcoat*Gr*Fr*Dr;
 // }
 
-
 // // 15 https://zhuanlan.zhihu.com/p/345518461
 
 // // decoupled smooth and rough mat.subsurface scattering
 // float3 SubsurfaceBurley15(float3 albedo, float roughness, float NoL, float NoV, float LoM) {
-//     // 
+//     //
 //     float f_l = Pow5(1.0 - NoL); // incident light refractiion
 //     float f_v = Pow5(1.0 - NoV); // outgoing light refraction
 //     float r_r =  2.0 * roughness * LoM * LoM;
--- horizon/assets/shaders/include/shading/shading_models.h
+++ horizon/assets/shaders/include/shading/shading_models.h
@@ -1,5 +1,5 @@
-#include "brdf_horizon.h"
 #include "brdf_disney.h"
+#include "brdf_horizon.h"
 #include "material_params_defination.hsl"
 
 // UnLit
@@ -20,7 +20,7 @@ float3 Brdf_Opaque_Default(MaterialProperties mat, BXDF bxdf) {
     return diffuse + specular;
 }
 
-//Masked()
+// Masked()
 
 // SubSurface
 
@@ -34,39 +34,18 @@ float3 Brdf_Opaque_Default(MaterialProperties mat, BXDF bxdf) {
 
 // cloth
 
-float3 Brdf_Sheen_Burley12(MaterialProperties mat, BXDF bxdf) {
-    float sheen_tint = 0.5, sheen = 0.5;
-    float D = NDF_GGX(mat.roughness2, bxdf.NoM);
-    float G = Vis_SmithGGXCombined(mat.roughness2, bxdf.NoV, bxdf.NoL);
-    float3 F = Fresnel_Schlick(mat.f0, bxdf.NoM);
+float3 Brdf_Standard_Burley12(MaterialProperties mat, BXDF bxdf) {
 
-    float3 kd = float3(1.0, 1.0, 1.0) - F;
-    kd *= 1.0 - mat.metallic;
+    // gbuffer save tangent
 
-    float3 diffuse = kd * (DiffuseBurley12(mat.albedo, mat.roughness, bxdf.VoM, bxdf.NoL, bxdf.NoV) + Sheen_Burley12(mat.albedo, sheen_tint, sheen, F));
+    // float aspect = SqrtFast2(1 - mat.anisotropic * 0.9);
+    // aspect = 1.0;
+    // float ax = max(0.001, Pow2(mat.roughness) / aspect);
+    // float ay = max(0.001, Pow2(mat.roughness) * aspect);
+    // float D = NDF_GTR2_Aniso(mat.roughness2, bxdf.XoM, bxdf.YoM, ax, ay);
 
-    float3 specular = D * G * F;
-
-    return diffuse + specular;
-}
-
-float3 Brdf_Subsurface_Burley12(MaterialProperties mat, BXDF bxdf) {
-    float subsurface = 0.5;
-    float D = NDF_GGX(mat.roughness2, bxdf.NoM);
-    float G = Vis_SmithGGXCombined(mat.roughness2, bxdf.NoV, bxdf.NoL);
-    float3 F = Fresnel_Schlick(mat.f0, bxdf.NoM);
-
-    float3 kd = float3(1.0, 1.0, 1.0) - F;
-    kd *= 1.0 - mat.metallic;
-
-    float3 diffuse = kd * (lerp(DiffuseBurley12(mat.albedo, mat.roughness, bxdf.VoM, bxdf.NoL, bxdf.NoV), SubsurfaceBurley12(mat.albedo, mat.roughness, bxdf.NoL, bxdf.NoV, bxdf.VoM), subsurface));
-
-    float3 specular = D * G * F;
-
-    return diffuse + specular;
-}
-
-float3 Brdf_ClearCoat_Burley12(MaterialProperties mat, BXDF bxdf) {
+    // float G = Vis_Smith_GGX_Aniso(bxdf.NoV, bxdf.XoV, bxdf.YoV, ax, ay) *
+    //           Vis_Smith_GGX_Aniso(bxdf.NoL, bxdf.XoL, bxdf.YoL, ax, ay);
     float D = NDF_GGX(mat.roughness2, bxdf.NoM);
     float G = Vis_SmithGGXCombined(mat.roughness2, bxdf.NoV, bxdf.NoL);
     float3 F = Fresnel_Schlick(mat.f0, bxdf.NoM);
@@ -79,4 +58,52 @@ float3 Brdf_ClearCoat_Burley12(MaterialProperties mat, BXDF bxdf) {
     float3 specular = D * G * F;
 
     return diffuse + specular;
+}
+
+float3 Brdf_Subsurface_Burley12(MaterialProperties mat, BXDF bxdf) {
+
+    // float aspect = SqrtFast2(1 - mat.anisotropic * 0.9);
+    // float ax = max(0.001, Pow2(mat.roughness) / aspect);
+    // float ay = max(0.001, Pow2(mat.roughness) * aspect);
+    // float D = NDF_GTR2_Aniso(mat.roughness2, bxdf.XoM, bxdf.YoM, ax, ay);
+
+    // float G = Vis_Smith_GGX_Aniso(bxdf.NoV, bxdf.XoV, bxdf.YoV, ax, ay) *
+    //           Vis_Smith_GGX_Aniso(bxdf.NoL, bxdf.XoL, bxdf.YoL, ax, ay);
+
+    float D = NDF_GGX(mat.roughness2, bxdf.NoM);
+    float G = Vis_SmithGGXCombined(mat.roughness2, bxdf.NoV, bxdf.NoL);
+
+    float3 F = Fresnel_Schlick(mat.f0, bxdf.NoM);
+
+    float3 kd = float3(1.0, 1.0, 1.0) - F;
+    kd *= 1.0 - mat.metallic;
+
+    float3 diffuse =
+        kd * (lerp(DiffuseBurley12(mat.albedo, mat.roughness, bxdf.VoM, bxdf.NoL, bxdf.NoV),
+                   SubsurfaceBurley12(mat.albedo, mat.roughness, bxdf.NoL, bxdf.NoV, bxdf.VoM), mat.subsurface));
+
+    float3 specular = D * G * F;
+
+    return diffuse + specular;
+}
+
+float3 Brdf_Cloth_Burley12(MaterialProperties mat, BXDF bxdf) {
+
+    float3 standard = Brdf_Standard_Burley12(mat, bxdf);
+
+    float f = Pow5(1.0 - bxdf.NoM);
+
+    float3 sheen_color = (1.0 - mat.metallic) * Sheen_Burley12(mat.albedo, mat.sheen_tint, mat.sheen, f);
+
+    return standard + sheen_color;
+}
+
+float3 Brdf_ClearCoat_Burley12(MaterialProperties mat, BXDF bxdf) {
+
+    float3 standard = Brdf_Standard_Burley12(mat, bxdf);
+
+    // clearcoat (ior = 1.5 -> F0 = 0.04)
+    float3 clearcoat = ClearCoat_Burley12(bxdf.NoL, bxdf.NoV, bxdf.NoM, mat.clearcoat, mat.clearcoat_gloss);
+
+    return standard + clearcoat;
 }
