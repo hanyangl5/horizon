0a5f368104b254561c950ad0ece089deaefade5b
v4vendeta <hy.lu@outlook.com>
2022-10-20 02:04:34 +0800

    s

--- horizon/assets/shaders/include/shading/brdf_disney.h
+++ horizon/assets/shaders/include/shading/brdf_disney.h
@@ -129,60 +129,6 @@ float3 ClearCoat_Burley12(float NoL, float NoV, float NoH, float clearcoat, floa
     return float3(0.25 * clearcoat * Dr * Fr * Gr);
 }
 
-// float3 BRDF(MaterialProperties_Disney12 mat, float3 L, float3 V, float3 N, float3 X, float3 Y )
-// {
-//     float NdotL = dot(N,L);
-//     float NdotV = dot(N,V);
-//     if (NdotL < 0 || NdotV < 0) return float3(0);
-
-//     float3 H = normalize(L+V);
-//     float NdotH = dot(N,H);
-//     float LdotH = dot(L,H);
-
-//     float3 Cdlin = mon2lin(mat.baseColor);
-//     float Cdlum = .3*Cdlin[0] + .6*Cdlin[1]  + .1*Cdlin[2]; // luminance approx.
-
-//     float3 Ctint = Cdlum > 0 ? Cdlin/Cdlum : float3(1); // normalize lum. to isolate hue+sat
-//     float3 Cspec0 = mix(mat.specular*.08*mix(float3(1), Ctint, mat.specularTint), Cdlin, mat.metallic);
-//     float3 Csheen = mix(float3(1), Ctint, mat.sheenTint);
-
-//     // Diffuse fresnel - go from 1 at normal incidence to .5 at grazing
-//     // and mix in diffuse retro-reflection based on mat.roughness
-//     float FL = SchlickFresnel(NdotL), FV = SchlickFresnel(NdotV);
-//     float Fd90 = 0.5 + 2 * LdotH*LdotH * mat.roughness;
-//     float Fd = mix(1.0, Fd90, FL) * mix(1.0, Fd90, FV);
-
-//     // Based on Hanrahan-Krueger brdf approximation of isotropic bssrdf
-//     // 1.25 scale is used to (roughly) preserve albedo
-//     // Fss90 used to "flatten" retroreflection based on mat.roughness
-//     float Fss90 = LdotH*LdotH*mat.roughness;
-//     float Fss = mix(1.0, Fss90, FL) * mix(1.0, Fss90, FV);
-//     float ss = 1.25 * (Fss * (1 / (NdotL + NdotV) - .5) + .5);
-
-//     // mat.specular
-//     float aspect = Pow2t(1-mat.anisotropic*.9);
-//     float ax = max(.001, Pow2(mat.roughness)/aspect);
-//     float ay = max(.001, Pow2(mat.roughness)*aspect);
-//     float Ds = GTR2_aniso(NdotH, dot(H, X), dot(H, Y), ax, ay);
-//     float FH = SchlickFresnel(LdotH);
-//     float3 Fs = mix(Cspec0, float3(1), FH);
-//     float Gs;
-//     Gs  = smithG_GGX_aniso(NdotL, dot(L, X), dot(L, Y), ax, ay);
-//     Gs *= smithG_GGX_aniso(NdotV, dot(V, X), dot(V, Y), ax, ay);
-
-//     // mat.sheen
-//     float3 Fsheen = FH * mat.sheen * Csheen;
-
-//     // mat.clearcoat (ior = 1.5 -> F0 = 0.04)
-//     float Dr = GTR1(NdotH, mix(.1,.001,mat.clearcoatGloss));
-//     float Fr = mix(.04, 1.0, FH);
-//     float Gr = smithG_GGX(NdotL, .25) * smithG_GGX(NdotV, .25);
-
-//     return ((1/_PI) * mix(Fd, ss, mat.subsurface)*Cdlin + Fsheen)
-//         * (1-mat.metallic)
-//         + Gs*Fs*Ds + .25*mat.clearcoat*Gr*Fr*Dr;
-// }
-
 // // 15 https://zhuanlan.zhihu.com/p/345518461
 
 // // decoupled smooth and rough mat.subsurface scattering
