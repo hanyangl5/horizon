6848d92f920387459e2c6af98a9412a228d8adfd
v4vendeta <45009841+v4vendeta@users.noreply.github.com>
2022-11-12 23:53:37 +0800

    fix microfacet brdf (#102)
    Co-authored-by: v4vendeta <hy.lu@outlook.com>

--- horizon/assets/shaders/include/shading/shading_models.h
+++ horizon/assets/shaders/include/shading/shading_models.h
@@ -10,10 +10,7 @@ float3 Brdf_Opaque_Default(MaterialProperties mat, BXDF bxdf) {
     float G = Vis_SmithGGXCombined(mat.roughness2, bxdf.NoV, bxdf.NoL);
     float3 F = Fresnel_Schlick(mat.f0, bxdf.NoM);
 
-    float3 kd = float3(1.0, 1.0, 1.0) - F;
-    kd *= 1.0 - mat.metallic;
-
-    float3 diffuse = kd * Diffuse_Lambert(mat.albedo);
+    float3 diffuse = Diffuse_Lambert(mat.albedo);
 
     float3 specular = D * G * F;
 
