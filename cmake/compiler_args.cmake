725237714a704955a8375f7c64f21cd23f32504c
hanyangl5 <v4vendeta.lhy@outlook.com>
2026-02-23 13:39:35 +0800

    update cmakelist

--- cmake/compiler_args.cmake
+++ cmake/compiler_args.cmake
@@ -1,7 +1,7 @@
 function(configure_compile_options target)
     target_compile_options(${PROJECT_NAME} PRIVATE -DNOMINMAX)
     if(MSVC)
-        target_compile_options(${target} PRIVATE /MP /permissive /w14640 /W3 /WX /external:anglebrackets /external:W0 /GR- /wd4100 /wd4101 /wd4189)
+        target_compile_options(${target} PRIVATE /MP /permissive /w14640 /W3 /WX /external:anglebrackets /external:W0 /GR- /utf-8 /wd4100 /wd4101 /wd4189)
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         target_compile_options(${target} PRIVATE -Wall -Wextra -Werror -Wshadow -pedantic -fms-extensions -Wno-language-extension-token -Wno-switch -Wno-missing-field-initializers -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function)
         if(ANDROID)
